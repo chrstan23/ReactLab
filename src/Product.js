1c36de15f51bbbb3e5c59b2746cbe595a4b41dbd
Kenneth Grant Allab <162018996+kennethgrant04@users.noreply.github.com>
2024-04-17 13:10:23 +0800

    I added Add to Cart to add total price and total products.

--- src/Product.js
+++ src/Product.js
@@ -1,14 +1,12 @@
-import React from "react";
+import React from 'react';
+import ProductInformation from './ProductInformation';
 
-function Product() {
-
-    return (
-        <div>
-            
-        </div>
-
-    )
-    
-}
+const Product = ({ name, price, description, addToCart }) => {
+  return (
+    <div>
+      <ProductInformation name={name} price={price} description={description} addToCart={() => addToCart(price)} />
+    </div>
+  );
+};
 
 export default Product;
